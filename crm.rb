96c16aaaeee8367393dff8511eee5ea0324e9a0c
Dominic Brugnano <dbgood@gmail.com>
2015-01-27 14:54:45 -0500

    Commited

--- crm.rb
+++ crm.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
 
 get '/' do 
+	@crm_app_name = "My CRM"
 	erb :index
+end
+
+get '/contacts' do 
+	@crm_app_name = "My CRM"
+	erb :contacts
+end
+
+get '/contacts/new' do 
+	@crm_app_name = "My CRM"
+	erb :new
 end
